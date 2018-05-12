5dabbc77448a9f0cae4642a4e30a601e19e6345d
Nigel Charman <nigel.charman.nz@gmail.com>
2018-05-12 16:33:49 +1200

    Fixed due to WebDriver upgrade.
    Now extending AbstractWebDriverEventListener to stop future breakage.

--- src/test/java/org/concordion/driver/selenium/SeleniumEventLogger.java
+++ src/test/java/org/concordion/driver/selenium/SeleniumEventLogger.java
@@ -3,16 +3,15 @@ package org.concordion.driver.selenium;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.events.AbstractWebDriverEventListener;
 import org.openqa.selenium.support.events.WebDriverEventListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-// Requires Selenium 2.23.0 or later to retrieve element names from WebElement.toString(). 
-public class SeleniumEventLogger implements WebDriverEventListener {
+public class SeleniumEventLogger extends AbstractWebDriverEventListener {
 
     final Logger logger = LoggerFactory.getLogger("selenium.events");
     private String oldValue;
-
 
     @Override
     public void beforeChangeValueOf(WebElement webElement, WebDriver webDriver, CharSequence[] charSequences) {
@@ -37,89 +36,6 @@ public class SeleniumEventLogger implements WebDriverEventListener {
     }
 
     @Override
-    public void afterClickOn(WebElement arg0, WebDriver arg1) {
-    }
-
-    @Override
-    public void afterFindBy(By arg0, WebElement arg1, WebDriver arg2) {
-        logger.debug("[{}] - found", arg0);
-    }
-
-    @Override
-    public void afterNavigateBack(WebDriver arg0) {
-        logger.debug("Navigated back");
-    }
-
-    @Override
-    public void afterNavigateForward(WebDriver arg0) {
-        logger.debug("Navigated forward");
-    }
-
-    @Override
-    public void afterNavigateTo(String arg0, WebDriver arg1) {
-        logger.debug("Navigated to '{}'", arg0);
-    }
-
-    @Override
-    public void afterScript(String arg0, WebDriver arg1) {
-        logger.debug("Ran script '{}'", arg0);
-    }
-
-    @Override
-    public void beforeSwitchToWindow(String windowName, WebDriver driver) {
-    }
-
-    @Override
-    public void afterSwitchToWindow(String windowName, WebDriver driver) {
-    }
-
-    @Override
-    public void beforeClickOn(WebElement arg0, WebDriver arg1) {
-        logger.debug("[{}] - clicked", getElementName(arg0));
-    }
-
-    @Override
-    public void beforeFindBy(By arg0, WebElement arg1, WebDriver arg2) {
-    }
-
-    @Override
-    public void beforeNavigateBack(WebDriver arg0) {
-    }
-
-    @Override
-    public void beforeNavigateForward(WebDriver arg0) {
-    }
-
-    @Override
-    public void beforeAlertAccept(WebDriver webDriver) {
-
-    }
-
-    @Override
-    public void afterAlertAccept(WebDriver webDriver) {
-
-    }
-
-    @Override
-    public void afterAlertDismiss(WebDriver webDriver) {
-
-    }
-
-    @Override
-    public void beforeAlertDismiss(WebDriver webDriver) {
-
-    }
-
-    @Override
-    public void beforeNavigateTo(String arg0, WebDriver arg1) {
-    }
-
-    @Override
-    public void beforeScript(String arg0, WebDriver arg1) {
-
-    }
-
-    @Override
     public void onException(Throwable arg0, WebDriver arg1) {
         logger.debug(arg0.getClass().getName(), arg0);
     }
@@ -133,15 +49,5 @@ public class SeleniumEventLogger implements WebDriverEventListener {
             }
         }
         return "unknown";
-    }
-
-    @Override
-    public void beforeNavigateRefresh(WebDriver driver) {
-
-    }
-
-    @Override
-    public void afterNavigateRefresh(WebDriver driver) {
-
     }
 }
