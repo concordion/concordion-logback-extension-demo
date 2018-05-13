e7782315552bf40753be5194c1531a10ce1569dc
Nigel Charman <nigel.charman.nz@gmail.com>
2018-05-13 12:36:08 +1200

    Removed unneccessary import

--- src/test/java/org/concordion/driver/selenium/SeleniumEventLogger.java
+++ src/test/java/org/concordion/driver/selenium/SeleniumEventLogger.java
@@ -1,10 +1,8 @@
 package org.concordion.driver.selenium;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.events.AbstractWebDriverEventListener;
-import org.openqa.selenium.support.events.WebDriverEventListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
